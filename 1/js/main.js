af5fa52005b41cc758c2d41bbfc7b15405e07965
UICyborg <uicyborg@gmail.com>
2019-10-13 01:39:03 +0300

    Мой первый коммит

---
+++ 1/js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+// var number = 5; // числа
+// var string = "Hello!"; // строка
+// var sym = Symbol(); // символ
+// var boolean = true; // логическое значение false/true
+// null; // это когда чего то в коде просто не существует
+// undefined; // получаем, если нет значения
+// var obj = {}; 
+// коллекция данных, 
+// структура которая используется для хранения любых данных
+
+// let persone = {
+//    name: "John",
+//    age: 25,
+//    isMarried: false
+// }
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// console.log(arr['1']);
+
+// //alert("Hello world!");
+// //let answer = confirm("Are you here?");
+
+
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object")
+// console.log(4 + + " - object")
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" == 2);
+
+
+// let isCheked = false;
+//     isClose = false;
+
+// console.log(isCheked || isClose);
